aecb4a30f2e6445c843964502254c917d0724b02
hirwavestine <gilhirwa22@gmail.com>
2019-08-22 09:27:36 +0200

    task creation 3 using function again

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ var task = Object.create(Object.prototype);
 task.title = "task creation again using a function";
 task.description = "learn how to create task";
 task.toString = function() {
-  return this.title;
+  return this.title + " " + this.description;
 };
 console.log(task.toString());
