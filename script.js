ee0f2247b6a09851638eb5be15c83dd88822b6e5
hirwavestine <gilhirwa22@gmail.com>
2019-08-22 09:33:09 +0200

    same way again to create task

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-var task = new Object();
-task.title = "task creation again using a function";
-task.description = "learn how to create task";
+var task = {
+  title: "task creation again using a function",
+  description: "learn how to create task"
+};
+
 task.toString = function() {
   return this.title + " " + this.description;
 };
