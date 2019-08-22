18cfa9d02975e1ea951d8846af78f1652c87f3b4
hirwavestine <gilhirwa22@gmail.com>
2019-08-22 10:55:06 +0200

    Demo: Define property

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-Object.defineProperty(obj, "name", {
-  value: "my name",
-  writable: true, //means I can create constants, something that can'y be changed.
-  enumerable: true, //either in a for in loop,or if I do the keys function then it will show up or it won't show up.
-  configurable: true // I am not allowed to set it again to something.
+var task = {
+  title: "task creation again using a function",
+  description: "learn how to create task"
+};
+Object.defineProperty(task, "toString", {
+  value: function() {
+    return this.title + " " + this.description;
+  },
+  writable: false,
+  enumerable: false,
+  configurable: false
 });
+Object.defineProperty(task, "toSring", {
+  enumerable: true
+});
+task.toString = "hi";
+console.log(Object.keys(task));
